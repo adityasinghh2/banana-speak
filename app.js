414e888f25ff6d26a16da4c1058781a7fb6f434a
adityasinghh2 <1132210493@mitwpu.edu.in>
2021-12-16 22:27:27 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var btnTranslate = document.querySelector("#btn-translate");
 
-btnTranslate.addEventListener("click", function clickEventHandler()  {
-    console.log("clicked!")
-})
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput)
+
+
+function clickHandler() {
+    console.log("clicked!");
+    console.log("input" , txtInput.value);
+};
+
+btnTranslate.addEventListener("click", clickHandler)  
+    
